85819dcf6dca4583e1e24c3183ee67345ae25042
dblancoformacion <dblancoformacion@gmail.com>
2019-07-03 14:58:17 +0200

    Primeras tablas

---
+++ querys.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE personas(
+  id_persona int AUTO_INCREMENT PRIMARY KEY,
+  persona varchar(31),
+  UNIQUE(persona)
+);
+CREATE TABLE lugares(
+  id_lugar int AUTO_INCREMENT PRIMARY KEY,
+  lugar varchar(31),
+  UNIQUE(lugar)
+);
