e64e93779a9ae48cd4ca367e14c7953e125afe87
dblancoformacion <dblancoformacion@gmail.com>
2019-07-03 16:48:59 +0200

    Divisiones

--- consultas.sql
+++ consultas.sql
@@ -9,7 +9,43 @@ SELECT persona,lugar
   JOIN lugares USING(id_lugar);
 
 -- (03) ¿Qué personas no viajarán?
+SELECT persona FROM viajaran RIGHT JOIN personas
+  ON personas.id_persona=viajaran.id_persona
+  WHERE viajaran.id_persona IS NULL;
+
 -- (04) Persona que más viajará
+SELECT MAX(persona) FROM personas; -- ¡¡Noooo!!
+
+SELECT persona FROM (
+    SELECT id_persona FROM viajaran GROUP BY 1
+      HAVING COUNT(*)=(
+        SELECT MAX(n) FROM (
+            SELECT id_persona,COUNT(*) n
+              FROM viajaran GROUP BY 1
+        ) c1
+    )
+  ) c2 JOIN personas USING(id_persona);
+
 -- (05) Lugar más visitado
--- (06) Destino al que ha ido todas las personas
--- (07) Persona que ha viajado a todos los destinos
+
+
+-- (06) Destinos al que viajarán todas las personas que viajan
+
+
+
+-- (07) Personas que viajarán a todos los destinos que se visitarán
+
+-- c1
+SELECT COUNT(DISTINCT id_lugar) FROM viajaran;
+-- c2
+SELECT id_persona FROM viajaran GROUP BY 1
+  HAVING COUNT(*)=(
+    SELECT COUNT(DISTINCT id_lugar) FROM viajaran
+);
+-- resultado
+SELECT persona FROM (
+    SELECT id_persona FROM viajaran GROUP BY 1
+      HAVING COUNT(*)=(
+        SELECT COUNT(DISTINCT id_lugar) FROM viajaran
+    )  
+  ) c3 JOIN personas USING(id_persona);
