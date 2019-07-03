49bf2cf0b6f6d7bdf27947e761636fbdb19651ed
dblancoformacion <dblancoformacion@gmail.com>
2019-07-03 15:10:08 +0200

    Diagrama SQL y tabla viajaran

--- querys.sql
+++ querys.sql
@@ -8,3 +8,11 @@ CREATE TABLE lugares(
   lugar varchar(31),
   UNIQUE(lugar)
 );
+CREATE TABLE viajaran(
+  id_viaje int AUTO_INCREMENT PRIMARY KEY,
+  id_persona int,
+  id_lugar int,
+  fecha date,
+  FOREIGN KEY(id_persona) REFERENCES personas(id_persona),
+  FOREIGN KEY(id_lugar) REFERENCES lugares(id_lugar)
+  )
