b9491237879806c458ceec1f21e6dbc7685827b3
dblancoformacion <dblancoformacion@gmail.com>
2019-07-03 15:41:21 +0200

    Consulta 02

--- consultas.sql
+++ consultas.sql
@@ -1,1 +1,6 @@
 ﻿-- (01) Lugares que no visitará nadie
+SELECT lugar FROM lugares LEFT JOIN viajaran
+  ON lugares.id_lugar=viajaran.id_lugar
+  WHERE viajaran.id_lugar IS null;
+
+-- (02) ¿A qué destinos va cada persona?
